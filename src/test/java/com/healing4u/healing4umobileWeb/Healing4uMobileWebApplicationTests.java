93290eae288a0cb052fc34358deb347c63b33b85
moonz <banan99@naver.com>
2021-10-13 17:51:11 +0900

    for build

--- src/test/java/com/healing4u/healing4umobileWeb/Healing4uMobileWebApplicationTests.java
+++ src/test/java/com/healing4u/healing4umobileWeb/Healing4uMobileWebApplicationTests.java
@@ -1,16 +1,6 @@
 package com.healing4u.healing4umobileWeb;
 
-import com.healing4u.healing4umobileWeb.data.repository.UserRepositoryTest;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class Healing4uMobileWebApplicationTests {
 
-	UserRepositoryTest userRepositoryTest;
-	@Test
-	void contextLoads() {
-		userRepositoryTest.select();
-	}
 
 }
