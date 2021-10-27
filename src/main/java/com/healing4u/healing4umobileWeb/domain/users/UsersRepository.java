3ee02ddaf5cac8b944c0b1284f1104463519ff2e
moonz <banan99@naver.com>
2021-10-27 22:44:12 +0900

    JPA-MySQL 연동 완료

--- src/main/java/com/healing4u/healing4umobileWeb/domain/users/UsersRepository.java
+++ src/main/java/com/healing4u/healing4umobileWeb/domain/users/UsersRepository.java
@@ -3,4 +3,5 @@ package com.healing4u.healing4umobileWeb.domain.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+
 }
